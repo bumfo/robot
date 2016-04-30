e7b7dba110ee45e2e14fa822437f949882c16b83
bumfo <bumfod@gmail.com>
2016-04-30 09:35:37 +0800

    rename entry

--- test.js
+++ test.js
@@ -36,37 +36,4 @@ that.loop(() => { // square
 // 	that.turn(10);
 // });
 
-// game.addSprite(that);
-
-class TestBot {
-	constructor(x, y, heading = 0) {
-		let bot = game.getRobotPeer();
-
-		bot.position.assign({
-			x: x,
-			y: y,
-		});
-
-		bot.heading = heading;
-
-		bot.loop(() => {
-			bot.fire(3);
-		});
-
-		game.addSprite(bot);
-	}
-}
-
-new TestBot(200, 200);
-let bot = game.getRobotPeer();
-bot.position.assign({
-	x: 600,
-	y: 200,
-});
-
-bot.loop(() => {
-	bot.turn(10);
-});
-
-game.addSprite(bot);
-// new TestBot(600, 200, Math.PI);
+game.addSprite(that);
